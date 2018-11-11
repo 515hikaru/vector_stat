d7417b609a8554bbd405f246e74e5d59c07ba1aa
515hikaru <12kojima.takahiro@gmail.com>
2018-11-11 22:19:55 +0900

    median implement

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     println!("Hello, world!");
-    let v = vec![100, 10, 33];
+    let v = vec![100, 10, 33, 34];
     println!("{:?}", v);
     println!("{}", mean(&v));
+    println!("{}", median(&v));
 }
 
 fn mean(vec: &Vec<i32>) -> f64 {
@@ -14,5 +15,15 @@ fn mean(vec: &Vec<i32>) -> f64 {
     sum as f64 / length
 }
 
-fn median(vec: &Vec<i32>) ->  f64 {
+fn median(vec: &Vec<i32>) -> f64 {
+    let mut sorted: Vec<i32> = Vec::new();
+    for item in vec.iter() {
+        sorted.push(*item);
+    }
+    sorted.sort_unstable();
+    let length = sorted.len();
+    if length % 2 == 0 {
+        return (sorted[length / 2] + sorted[length / 2 - 1]) as f64 / 2 as f64
+    }
+    sorted[length / 2] as f64
 }
