2c9442aa73e2ec8ea4e4b66f83fb7431db257fae
515hikaru <12kojima.takahiro@gmail.com>
2018-11-11 22:08:34 +0900

    mean function implement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+    let v = vec![100, 10, 33];
+    println!("{:?}", v);
+    println!("{}", mean(&v));
+}
+
+fn mean(vec: &Vec<i32>) -> f64 {
+    let mut sum: i32 = 0;
+    for item in vec {
+        sum += item
+    }
+    let length = vec.len() as f64;
+    sum as f64 / length
+}
+
+fn median(vec: &Vec<i32>) ->  f64 {
 }
