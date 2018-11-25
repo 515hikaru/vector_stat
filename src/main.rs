d3c884dd69de41127981019676ac996e51c47203
515hikaru <12kojima.takahiro@gmail.com>
2018-11-26 00:20:09 +0900

    標準入力を受け取るように修正

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
 use std::collections::HashMap;
+use std::io;
 
 fn main() {
-    let v = vec![100, 100, 100, 1, 50];
-    println!("{:?}", v);
-    println!("平均: {}", mean(&v));
-    println!("中央値: {}", median(&v));
-    println!("最頻値: {}", mode(&v));
+    println!("半角スペース区切りで整数を入力してください。");
+    loop {
+        let mut s = String::new();
+        io::stdin().read_line(&mut s).expect("Failed to read line.");
+        let mut v: Vec<i32> = Vec::new();
+        let mut all_number = true;
+        for w in s.split_whitespace() {
+            let i: Option<i32> = match w.trim().parse() {
+                Ok(j) => Some(j),
+                Err(_) => None,
+            };
+            match i {
+                Some(j) => v.push(j),
+                None => {
+                    all_number = false;
+                    break;
+                }
+            }
+        }
+        if all_number == false {
+            println!("入力された値を整数に変換することに失敗しました。入力を確認してください。");
+            continue;
+        }
+        println!("入力: {:?}", v);
+        println!("平均: {}", mean(&v));
+        println!("中央値: {}", median(&v));
+        println!("最頻値: {}", mode(&v));
+    }
 }
 
 fn mean(vec: &Vec<i32>) -> f64 {
@@ -25,7 +49,7 @@ fn median(vec: &Vec<i32>) -> f64 {
     sorted.sort_unstable();
     let length = sorted.len();
     if length % 2 == 0 {
-        return (sorted[length / 2] + sorted[length / 2 - 1]) as f64 / 2 as f64
+        return (sorted[length / 2] + sorted[length / 2 - 1]) as f64 / 2 as f64;
     }
     sorted[length / 2] as f64
 }
@@ -44,8 +68,8 @@ fn mode(vec: &Vec<i32>) -> i32 {
     }
     for (key, val) in map.iter() {
         if *val == m {
-            return **key
+            return **key;
         }
     }
-    return -1
+    return -1;
 }
